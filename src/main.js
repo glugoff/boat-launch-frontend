577d7a17533010122b438fbba73b962a15f94f2b
glugoff <glugoff@gmail.com>
2025-02-15 19:01:36 +0300

    fix import leaflet css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';  // Импортируем Pinia
+import 'leaflet/dist/leaflet.css';
 
 const app = createApp(App);
 
